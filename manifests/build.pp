116f4ec6c265014c2a349137efd213e44e8fea34
Mariusz Gronczewski <xani666@gmail.com>
2014-11-02 16:10:53 +0100

    add a simple builder for containers

---
+++ manifests/build.pp
@@ -0,0 +1,25 @@
+# == Define: docker:build
+#
+# Create image from provided Dockerfile
+#
+
+define docker::build(
+  $image = $title,
+  $tag   = 'latest',
+  $content,
+  $tmp = "/tmp/docker_build-${title}",
+  ) {
+  validate_re($image, '^[\S]*$')
+  file {$tmp:
+      ensure => directory,
+      owner  => root,
+      group  => docker,
+      mode   => 750,
+  }
+  exec { "build-${image}-${tag}":
+      cwd => $tmp,
+      command => "docker build -t ${image}/${tag}",
+      unless => "docker.io images ${image} | grep ${image} | grep -q ${tag}",
+  }
+
+}
